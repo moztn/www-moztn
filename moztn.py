1a84b47089e7eef1bca2f5f7b082a57283559a4c
Alexandre BM <s@rednaks.tn>
2015-03-21 12:56:47 +0100

    Adding routes

--- moztn.py
+++ moztn.py
@@ -12,6 +12,26 @@ app = Flask(__name__)
 def home():
     return render_template('home/index.html')
 
+@app.route('/contribute')
+def contribute():
+    return 'Please contribute to this page not yet done :('
+
+@app.route('/resources')
+def resources():
+    return 'Not yet done'
+
+@app.route('/join')
+def join():
+    return 'Not yet done'
+
+@app.route('/about')
+def about():
+    return 'Not yet done'
+
+@app.route('/contact')
+def contact():
+    return 'Not yet done'
+
 
 
 if __name__ == '__main__':
